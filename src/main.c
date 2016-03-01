48155a76c63c75bd384224fdc354ad1773f09faa
atom <atom@atom>
2016-03-01 07:50:00 +0100

    continue

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include "cgl.h"
+#include "graphics/shader.h"
+#include "graphics/cgprogram.h"
+#include "graphics/scene.h"
+
+t_cgl_cgprogram	*p = 0;
 
 int			game_run(t_cgl_engine *engine)
 {
+	printf("in\n");
+	cgl_tmp_draw(p);
 	return (0);
 }
 
@@ -15,6 +22,20 @@ int			main(int ac, char **av)
 		cgl_engine_system_deinit();
 		return (ret);
 	}
+	t_cgl_shader	*v = cgl_graphics_shader_create("Shaders/couleur2D.vert", CGL_SHADER_VERTEX);
+	if (!v)
+		return (-1);
+	t_cgl_shader	*f = cgl_graphics_shader_create("Shaders/couleur2D.frag", CGL_SHADER_FRAGMENT);
+	if (!f)
+		return (-1);
+	p = cgl_graphics_cgprogram_create(v, f);
+	if (!p)
+		return (-1);
+	cgl_graphics_cgprogram_bind_location(p, 0, "in_Vertex");
+	cgl_graphics_cgprogram_bind_location(p, 1, "in_Color");
+	cgl_graphics_cgprogram_bind_location(p, 2, "in_TexCoord0");
+	if (cgl_graphics_cgprogram_load(p) == -1)
+		return (-1);
 	ret = cgl_engine_system_run(game_run);
 	cgl_engine_system_deinit();
 	return (ret);
